4374e80a8e6d0bd0d9a22de841d2c58f4a34bf43
muhammadhamzafahim1123-byte <muhammadhamzafahim1123-byte@users.noreply.github.com>
2026-05-16 20:04:26 +0500

    support google callback route

---
+++ app/api/auth/callback/google/route.ts
@@ -0,0 +1,6 @@
+import { GET as googleCallback } from "../../google/callback/route";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+export const GET = googleCallback;
